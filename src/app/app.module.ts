018721bf4cf6d1afaf4e3989e70c2f52d2f989d2
Dislani <dismelo16@gmail.com>
2022-09-06 05:10:29 -0300

    Criação dos componentes Inicio e Tema e preparação dos mesmos para receber o consumo da Api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { FormsModule } from '@angular/forms';
+import { InicioComponent } from './inicio/inicio.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { TemaComponent } from './tema/tema.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,8 @@ import { FormsModule } from '@angular/forms';
     RodapeComponent,
     EntrarComponent,
     CadastrarComponent,
+    InicioComponent,
+    TemaComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,7 +29,10 @@ import { FormsModule } from '@angular/forms';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
