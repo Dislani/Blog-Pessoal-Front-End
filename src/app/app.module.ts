65abf0df84836ac5e59c304fd2b8e18a6891eaff
Dislani <dismelo16@gmail.com>
2022-09-14 11:51:10 -0300

    Ordenando as postagens pelas publicações recentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,7 +43,8 @@ import { OrderModule } from 'ngx-order-pipe';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    OrderModule
   
     
     
